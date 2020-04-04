5de3f5b7394e8cf14d213fdb91490ac409911bf9
jy.you <wynn1275@gmail.com>
2020-04-04 15:52:48 +0900

    setup boot project

---
+++ src/test/java/com/wynnn/ipfilter/IpFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wynnn.ipfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
